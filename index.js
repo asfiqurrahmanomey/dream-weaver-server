ecccae9f52ad6dea6106d63e601eccf70e1bdddf
asfiqurrahmanomey <omeyjkt@gmail.com>
2022-11-09 03:11:56 +0600

    .env

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
-const port  = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9vhsktv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Dream Weaver Server is Running')
 })
 
-app.listen(port,  () => {
+app.listen(port, () => {
     console.log('Dream Weaver Server is running on port', port);
 })
