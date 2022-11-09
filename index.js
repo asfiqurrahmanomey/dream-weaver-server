7963ad2eb5a4d41998379bb53306eef8019933c0
asfiqurrahmanomey <omeyjkt@gmail.com>
2022-11-10 03:48:15 +0600

    mongo_cont

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -19,22 +19,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('dreamWeaver').collection('services');
-
+        
+        // To get 3 Services 
         app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // Top Get All services
+        app.get('/allServices', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
     }
     finally {
 
     }
 }
-
 run().catch(err => console.error(err));
-
-
 
 app.get('/', (req, res) => {
     res.send('Dream Weaver Server is Running')
